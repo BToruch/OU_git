21a09badfb4d943ac56df431565b17666aee95d3
BToruch <235999@edu.p.lodz.pl>
2021-01-22 09:04:53 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CLion.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CLion.exe"
+  "CLion.exe.manifest"
+  "CLion.lib"
+  "CLion.pdb"
+  "CMakeFiles/CLion.dir/main.c.obj"
+  "CMakeFiles/CLion.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/CLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
