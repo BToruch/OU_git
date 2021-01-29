f9d90a47292afa59ca84dd942526e66bd44e2baf
BToruch <235999@edu.p.lodz.pl>
2021-01-29 08:13:26 +0100

    Zmiana mnozenia w metodzie policz

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+int Policz(int a, int b)
+{
+    return a+b;
+}
+
 int main()
 {
     printf("Hello, world!\n");
+    printf("Hello, new world!\n");
+    printf("2+3=%d", Policz(2, 3));
     return 0;
 }
